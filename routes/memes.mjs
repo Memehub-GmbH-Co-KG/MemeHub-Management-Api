f08f68d2665c30cb6fd9f270574c3b77d9050eaa
bertye <baert.ye@gmail.com>
2020-03-06 19:26:16 +0100

    fixed meme date filter

--- routes/memes.mjs
+++ routes/memes.mjs
@@ -1,16 +1,17 @@
 import { validateSession } from '../lib/session.mjs';
 import express from 'express';
+import moment from 'moment';
 import { getMemes } from '../db/memes.mjs';
 import { markMemeAsRepost } from '../bot/repost.mjs';
 
 const router = express.Router()
 
 router.get('/memes', validateSession, async (req, res) => {
-    let amount = parseInt(req.query.amount);
-    const olderThan = req.query.olderThan;
-    const newerThan = req.query.newerThan;
-    if (typeof amount !== 'number' || amount < 0 || amount > 50)
-        amount = 20;
+    let amount = parseInt(req.query.amount) || 10;
+    const olderThan = req.query.olderThan ? moment(req.query.olderThan).toDate() : null;
+    const newerThan = req.query.newerThan ? moment(req.query.newerThan).toDate() : null;
+    if (amount < 0 || amount > 50) amount = 10;
+
     const memes = await getMemes(amount, olderThan, newerThan);
     res.json(memes);
 });
